08990f6f2a5a9e061f16bb017fd74b8c6bb53ec9
atramors <blackworkarts@gmail.com>
2020-07-08 00:20:41 +0300

    Add some improvements.

--- sorter.py
+++ sorter.py
@@ -16,18 +16,6 @@ for i in range(1, 13):
 for fls in os.listdir(photos):
     source = "/Users/atramors/Desktop/photos" + "/" + fls
     month_checker = datetime.datetime.fromtimestamp(os.path.getmtime(source)).month
-    destination = {
-        1: "/Users/atramors/Desktop/sorted_photos/1.2020_Fuji",
-        2: "/Users/atramors/Desktop/sorted_photos/2.2020_Fuji",
-        3: "/Users/atramors/Desktop/sorted_photos/3.2020_Fuji",
-        4: "/Users/atramors/Desktop/sorted_photos/4.2020_Fuji",
-        5: "/Users/atramors/Desktop/sorted_photos/5.2020_Fuji",
-        6: "/Users/atramors/Desktop/sorted_photos/6.2020_Fuji",
-        7: "/Users/atramors/Desktop/sorted_photos/7.2020_Fuji",
-        8: "/Users/atramors/Desktop/sorted_photos/8.2020_Fuji",
-        9: "/Users/atramors/Desktop/sorted_photos/9.2020_Fuji",
-        10: "/Users/atramors/Desktop/sorted_photos/10.2020_Fuji",
-        11: "/Users/atramors/Desktop/sorted_photos/11.2020_Fuji",
-        12: "/Users/atramors/Desktop/sorted_photos/12.2020_Fuji"
-    }
-    shutil.move(source, destination[month_checker])
+    destination = f"/Users/atramors/Desktop/sorted_photos/{month_checker}.2020_Fuji"
+
+    shutil.move(source, destination)
