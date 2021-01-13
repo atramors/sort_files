bb01520ccbec87f3d11656b822eb7fc5577a8449
Roman D <blackworkarts@gmail.com>
2021-01-13 18:18:41 +0200

    Prettifying

--- sorter.py
+++ sorter.py
@@ -1,13 +1,15 @@
+# !/bin/python3
+
 import os
 import shutil
 import datetime
-import pathlib
 
 # Create folder for sorted files.
 
-
-files = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop/unsorted/')
-sorted_files = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop/sorted_photos/')
+unsorted_files = os.path.join(os.path.join(os.path.expanduser('~')),
+                              'Desktop/unsorted/')
+sorted_files = os.path.join(os.path.join(os.path.expanduser('~')),
+                            'Desktop/sorted_photos/')
 
 os.mkdir(sorted_files)
 
@@ -15,8 +17,8 @@ os.mkdir(sorted_files)
 # Loop for month, year creation check.
 # Create folders named like months and moving files there.
 
-for fls in os.listdir(files):
-    source = files + fls
+for fls in os.listdir(unsorted_files):
+    source = unsorted_files + fls
     check = datetime.datetime.fromtimestamp(os.path.getmtime(source))
     destination = sorted_files + f"/{check.month}.{check.year}_Fuji/"
 
@@ -24,5 +26,4 @@ for fls in os.listdir(files):
         os.mkdir(destination)
     except FileExistsError:
         pass
-    
     shutil.move(source, destination)
